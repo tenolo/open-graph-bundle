afd54abe18fecb463d16bf7ddf98f5b418b52e48
Manuel Dalla Lana <manuel@dallalana.it>
2019-07-31 00:03:59 +0200

    Fix deprecation notice from TreeBuilder

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -19,9 +19,6 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('tenolo_open_graph');
-
-        return $treeBuilder;
+        return new TreeBuilder('tenolo_open_graph');
     }
 }
